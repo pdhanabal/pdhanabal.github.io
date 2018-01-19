fa344b00e9a125c09cad4bbfbf0400f79de916a6
pdhanabal <PDHANABAL@yahoo.com>
2018-01-19 14:55:38 -0500

    Updated Date

--- js/scripts.js
+++ js/scripts.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
 $(document).ready(function() {
 	"use strict";
 	$("#countdown").countdown({
-		date: "12 june 2015 12:00:00", /** Enter new date here **/
+		date: "31 March 2018 4:30:00", /** Enter new date here **/
 		format: "on"
 	},
 	function() {
